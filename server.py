68cbd7d842886598c7f1f166e99a4b26b161cb4a
Aviv-Terasky <aviv@terasky.com>
2022-05-31 00:35:36 +0300

    create app with flask

---
+++ server.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, jsonify
+
+MORSE_CODE_DICT = { 'A':'.-', 'B':'-...',
+                    'C':'-.-.', 'D':'-..', 'E':'.',
+                    'F':'..-.', 'G':'--.', 'H':'....',
+                    'I':'..', 'J':'.---', 'K':'-.-',
+                    'L':'.-..', 'M':'--', 'N':'-.',
+                    'O':'---', 'P':'.--.', 'Q':'--.-',
+                    'R':'.-.', 'S':'...', 'T':'-',
+                    'U':'..-', 'V':'...-', 'W':'.--',
+                    'X':'-..-', 'Y':'-.--', 'Z':'--..',
+                    '1':'.----', '2':'..---', '3':'...--',
+                    '4':'....-', '5':'.....', '6':'-....',
+                    '7':'--...', '8':'---..', '9':'----.',
+                    '0':'-----', ', ':'--..--', '.':'.-.-.-',
+                    '?':'..--..', '/':'-..-.', '-':'-....-',
+                    '(':'-.--.', ')':'-.--.-'}
+
+def encrypt(message):
+    cipher = ''
+    for letter in message:
+        if letter != ' ':
+            cipher += MORSE_CODE_DICT[letter] + ' '
+        else:
+            cipher += ' '
+ 
+    return cipher
+
+app = Flask(__name__)
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    print(request)
+    mors = encrypt(request.remote_addr)
+    return jsonify({'ip_translate_to_mors': mors}), 200
+
+if __name__ == '__main__':
+ app.run(host='0.0.0.0', debug=True, port=8080)
